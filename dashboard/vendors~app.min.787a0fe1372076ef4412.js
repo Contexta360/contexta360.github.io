315bc768c524905e438577fe856f705e93fc8136
kuus <kunderikuus@gmail.com>
2020-02-19 12:25:56 +0100

    Updates

--- dashboard/vendors~app.min.787a0fe1372076ef4412.js
+++ dashboard/vendors~app.min.787a0fe1372076ef4412.js
@@ -2,7 +2,7 @@
 /*!
  * Contexta 360 Dashboard v4.3.0-rc.1 (https://contexta360.com/)
  * Copyright (c) 2017-2020 Contexta B.V.
- * @license ISC (Last change on: 19/02/2020 10:36:07)
+ * @license ISC (Last change on: 19/02/2020 12:24:10)
  * 
  * Frontend developed by kuus <kunderikuus@gmail.com> (http:kunderikuus.net)
  */
