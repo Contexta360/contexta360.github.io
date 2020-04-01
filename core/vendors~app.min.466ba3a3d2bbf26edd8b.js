5ff6f963ca2a508e1c17acf49ee49c7b8da44f38
kuus <kunderikuus@gmail.com>
2020-04-01 09:02:09 +0200

    Updates

--- core/vendors~app.min.466ba3a3d2bbf26edd8b.js
+++ core/vendors~app.min.466ba3a3d2bbf26edd8b.js
@@ -2,7 +2,7 @@
 /*!
  * Contexta 360 Core v0.0.1 (https://contexta360.com/)
  * Copyright (c) 2020 Contexta B.V.
- * @license ISC (Last change on: 01/04/2020 08:58:49)
+ * @license ISC (Last change on: 01/04/2020 09:01:48)
  * 
  * Frontend developed by kuus <kunderikuus@gmail.com> (http:kunderikuus.net)
  */
