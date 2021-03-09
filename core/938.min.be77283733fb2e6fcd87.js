b94ab1b09a1986c3fb5f29ec4fbdccbb012ae7ac
kuus <kunderikuus@gmail.com>
2021-03-09 21:03:14 +0100

    Updates

--- core/938.min.be77283733fb2e6fcd87.js
+++ core/938.min.be77283733fb2e6fcd87.js
@@ -2,7 +2,7 @@
 /*!
  * Core v1.2.1 (https://contexta360.com/)
  * Copyright (c) 2020-2021 Contexta B.V.
- * @license ISC (Last change on: 09/03/2021 20:46:02)
+ * @license ISC (Last change on: 09/03/2021 21:02:48)
  *
  * Frontend developed by kuus <kunderikuus@gmail.com> (https://kuus.github.io)
  */
