ace31af9b465b2b49379b4f279a033212100ece1
kuus <kunderikuus@gmail.com>
2023-10-02 15:39:08 +0200

    chore(deploy): core staging

--- core/assets/479.min.26709151c6df431f1f04.js
+++ core/assets/479.min.26709151c6df431f1f04.js
@@ -1,7 +1,7 @@
 /*!
  * Contexta360 Core v3.0.0-alpha.1 (https://contexta360.com)
  * Copyright (c) 2020-2023 Contexta B.V.
- * @license ISC (Last change on: 30/09/2023 09:44:30)
+ * @license ISC (Last change on: 02/10/2023 15:37:25)
  *
  * Frontend developed by kuus <kunderikuus@gmail.com> (https://kuus.github.io)
  */
